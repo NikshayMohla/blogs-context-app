332d25d7067dd7909d2e9dfde96d9ef5201ac83d
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-16 14:09:17 +0530

    Context API Setup

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Header = () => {
+const Pagination = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Pagination
